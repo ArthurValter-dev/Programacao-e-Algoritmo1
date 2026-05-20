b510a35ad08134ef29a33edde780745909819b16
Arthur Valter Dos Santos Ferreira <arthurvalterdossantosferr@gmail.com>
2026-05-20 11:37:09 -0300

    Add files via upload

---
+++ Vertor_e_Matrizes/src/vertor_e_matrizes/Nome.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vertor_e_matrizes;
+
+/**
+ *
+ * @author Admin
+ */
+class Nome {
+    
+}
---
+++ Vertor_e_Matrizes/src/vertor_e_matrizes/Vertor_e_Matrizes.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vertor_e_matrizes;
+
+import java.util.Scanner;
+public class Vertor_e_Matrizes {
+
+
+    public static void main(String[] args) {
+        // Esse codigo será sobre vetor e matrizes
+        
+        Scanner leia = new Scanner(System.in);
+        
+        System.out.print("Quantos nomes voce deseja acidionar:");
+        int Quan = leia.nextInt();
+        System.out.println("Digite nomes de acordo da com a quantidade");
+        while(Quan >=Quan){
+            
+            String NNome;
+            String Nome[]= {NNome=leia.nextLine(),};
+        
+       // String NNome;
+       // String Nome []= {NNome=leia.nextLine(),NNome=leia.nextLine(),NNome=leia.nextLine(),};
+        
+        
+        int n;
+        for(n=0;n<=4;n++){
+           System.out.println("O nome dos alunos sao: "+Nome[Quan]);
+        }
+    }
+    }
+}
