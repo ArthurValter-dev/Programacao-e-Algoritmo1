c4cb502aebb459958a530e52164aae181004d079
Arthur Valter Dos Santos Ferreira <arthurvalterdossantosferr@gmail.com>
2026-05-20 12:29:36 -0300

    Add files via upload

---
+++ EXE.1ArthurValter/src/exe/pkg1arthurvalter/EXE1ArthurValter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exe.pkg1arthurvalter;
+import javax.swing.JOptionPane;
+public class EXE1ArthurValter {
+
+    public static void main(String[] args) {
+        
+        //exercicio para exibir todos numeros de 1 e 20 pares
+        int i = 1;
+        int p = 0, n = p +2;
+        while(i <11){
+                System.out.println(n);
+                i++;
+                n= n +2;
+            }
+        }
+    }
