855447a717f33cbd7d5bfd74231f7f714d56aa51
Arthur Valter Dos Santos Ferreira <arthurvalterdossantosferr@gmail.com>
2026-05-25 13:43:48 -0300

    Add files via upload

--- ArthurValterEnt01/src/arthurvalterent01/ArthurValterEnt01.java
+++ ArthurValterEnt01/src/arthurvalterent01/ArthurValterEnt01.java
@@ -10,23 +10,21 @@ public class ArthurValterEnt01 {
     public static void main(String[] args) {
         // Apresente os números impares existentes entre 75 e 197 inclusive
         
+        
         int n1 = 75, n2 = 197;
+        //Apresenta função do codigo
         JOptionPane.showMessageDialog(null,"Esse codigo ira mostra os numeros impares entre 75");
+        
+        //Declarando variaveis
+        
+        //O contador servira para mostrar o numero impar em quetão
         int cont = 1;
         while(n1 <=n2){
             n1 = n1+2;
             JOptionPane.showMessageDialog(null,"O "+cont+"° e "+n1);
             
+            //ao n1 chegar ao valor de n2 o laço acabara
             if(n1 == n2){
-                break;
-            }
-            cont++;
-        }
-while(n2 <=n1){
-            n2 = n2+2;
-            JOptionPane.showMessageDialog(null,"O "+cont+"° e "+n2);
-            
-            if(n2 == n1){
                 break;
             }
             cont++;
